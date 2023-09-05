81d64ba2bc26c38d266856a103f01ef3bdf913a8
Rafael da Silva Santos <05202198155@faqi.edu.br>
2023-09-05 03:35:09 -0300

    :sparkles: feat: imagem.html added to urls and view
    Added the url path for the imagem.
    Added request function to views.

--- galery/urls.py
+++ galery/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from galery.views import index
+from galery.views import index, imagem
 
 
 urlpatterns = [
-    path('', index)
+    path('', index),
+    path('imagem/', imagem),
 ]
