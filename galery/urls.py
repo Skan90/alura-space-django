9ed4a5b8dd21d9e839f95a59dc4bf2555c1d579a
Rafael da Silva Santos <05202198155@faqi.edu.br>
2023-09-05 04:05:23 -0300

    :bug: fix: Misspeling 'gallery' fixed.
    Fixed the name for the app from 'galery' to the correct speeling 'gallery' in directory and references.

--- galery/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from galery.views import index, imagem
-
-
-urlpatterns = [
-    path('', index),
-    path('imagem/', imagem),
-]
--- galery/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-
-def index(request):
-    return render(request, 'galery/index.html')
-
-
-def imagem(request):
-    return render(request, 'galery/imagem.html')
