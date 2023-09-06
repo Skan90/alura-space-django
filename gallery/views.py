9daa7e5e4e16f98a85a8fa66a06f6010c0d75035
Rafael da Silva Santos <05202198155@faqi.edu.br>
2023-09-06 11:44:45 -0300

    :construction: WIP: Dynamically getting info for <li>
    Dynamically getting from a simple dictionary data to fill a list in HTML

--- gallery/views.py
+++ gallery/views.py
@@ -2,7 +2,15 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'gallery/index.html')
+
+    data = {
+        1: {"name": "Nebulosa Eta Carinae",
+            "description": "webbtelecope.org / NASA / James Webb Space Telescope"},
+        2: {"name": "Galaxy NGC 1079",
+            "description": "nasa.org / NASA / Hubble Space Telescope"},
+    }
+
+    return render(request, 'gallery/index.html', {"cards": data})
 
 
 def imagem(request):
