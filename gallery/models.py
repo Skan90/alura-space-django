c4e134dea93e5c2acbe6561437302e8f23c1be13
Rafael da Silva Santos <05202198155@faqi.edu.br>
2023-09-10 07:48:23 -0300

    :sparkles: feat: Creating first Model
    Creating model for Photography

--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Photography(models.Model):
+
+    CATEGORY_CHOICES = (
+        ("NEBULA", "Nebula"),
+        ("START", "Star"),
+        ("GALAXY", "Galaxy"),
+        ("PLANET", "Planet"),
+    )
+
+    name = models.CharField(max_length=100, null=False, blank=False)
+    credits = models.CharField(max_length=200, null=False, blank=False)
+    category = models.CharField(max_length=100, choices=CATEGORY_CHOICES, default='')
+    description = models.TextField(null=False, blank=False)
+    photo = models.CharField(max_length=500, null=False, blank=False)
+
+    def __str__(self):
+        return f"Photography [name={self.name}]"
